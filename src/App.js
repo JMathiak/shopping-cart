bfa713c7f9e70ff22ed45916d7e56a0f59b2c9d4
Josh Mathiak <jmathiak4@gmail.com>
2022-09-19 13:53:57 -0500

    Set up header naviagtion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import React, { useState } from "react";
 
 import Test from "./Test";
 import Header from "./components/Header";
+import BrowseShop from "./components/BrowseShop";
+import ShoppingCart from "./components/ShoppingCart";
 function App() {
   const [text, setText] = useState("DC");
-  const [text2, setText2] = useState("Sleepy");
+  const [cart, setCart] = useState([]);
+
   return (
     <div className="Container">
-      <Header />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Test text={text} />} />
-          <Route path="/home" element={<Test text={text2} />} />
-        </Routes>
-      </Router>
+      <Header cartSize={cart.length} />
+      <Routes>
+        <Route path="/" element={<Test text={text} />} />
+        <Route path="/shop" element={<BrowseShop />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
-
-const Header = () => {
-  return <div>Header</div>;
-};
+import { useNavigate } from "react-router-dom";
+import "../styles/Header.css";
+function Header(props) {
+  const navigate = useNavigate();
+  return (
+    <div className="header">
+      <div className="link-holder">
+        <div
+          className="link"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Home
+        </div>
+        <div
+          className="link"
+          onClick={() => {
+            navigate("/shop");
+          }}
+        >
+          Browse Shop
+        </div>
+        <div
+          className="link"
+          onClick={() => {
+            navigate("/cart");
+          }}
+        >
+          View Cart
+        </div>
+      </div>
+      <div>{props.cartSize}</div>
+    </div>
+  );
+}
 
 export default Header;
