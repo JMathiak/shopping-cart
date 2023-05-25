154831c8baac4978991db1916832901cb519a1e6
Josh Mathiak <jmathiak4@gmail.com>
2023-05-25 13:43:59 -0500

    added title for website + adjusted cart/size

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,48 +6,50 @@ import "../styles/Header.css";
 function Header(props) {
   const navigate = useNavigate();
   return (
-    <nav className="header">
-      <ul className="link-holder">
-        <li>
-          <Link
-            className="link"
-            to="home"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/");
-            }}
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="link"
-            to="shop"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/shop");
-            }}
-          >
-            Browse Shop
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="link"
-            to="cart"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/cart");
-            }}
-          >
-            <FontAwesomeIcon icon={faCartShopping} />
-          </Link>
-        </li>
-
-        <li>{props.cartSize}</li>
-      </ul>
-    </nav>
+    <header className="header">
+      <h1 className="website-title">Shua's Valorant Gallery</h1>
+      <nav>
+        <ul className="link-holder">
+          <li>
+            <Link
+              className="link"
+              to="home"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/");
+              }}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link"
+              to="shop"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/shop");
+              }}
+            >
+              Shop
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link"
+              to="cart"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/cart");
+              }}
+            >
+              <FontAwesomeIcon className="cart-icon" icon={faCartShopping} />
+              {props.cartSize}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
