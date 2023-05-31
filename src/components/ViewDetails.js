4b8102e9e7a1ee4fa79ee529c968eeaac0713b22
Josh Mathiak <jmathiak4@gmail.com>
2023-05-31 11:38:59 -0500

    added add to cart functionality

--- src/components/ViewDetails.js
+++ src/components/ViewDetails.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
-const ViewDetails = () => {
+const ViewDetails = ({ cart, setCart }) => {
   const location = useLocation();
+  console.log(location.state);
   const { name } = location.state;
   const { price } = location.state;
   const { type } = location.state;
   const { image } = location.state;
+  const { allItems } = location.state;
+  const { itemNumber } = location.state;
+
+  const addToCart = () => {
+    if (cart.filter((x) => x.itemNumber === itemNumber).length > 0) {
+      let workingArr = [...cart];
+      let index = workingArr.findIndex((e) => e.itemNumber === itemNumber);
+      // let itemQuant = parseInt(workingArr[index].quantity) + parseInt(quant);
+      // workingArr[index].quantity = itemQuant;
+      setCart(workingArr);
+    } else {
+      let item = allItems.find((x) => x.itemNumber === itemNumber);
+      let arrItem = {
+        name: item.name,
+        image: item.image,
+        cost: item.cost,
+        type: item.type,
+        itemNumber: item.itemNumber,
+      };
+      setCart([...cart, arrItem]);
+    }
+
+    // setQuant(1);
+  };
   return (
     <div>
       {name}, {price} VP, {type}
       <img src={image} alt="Gun"></img>
+      <button onClick={addToCart}>Add to Cart</button>
     </div>
   );
 };
