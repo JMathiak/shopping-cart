8328f028f3b2a7ad4dd383f6ecc0fad53e375b44
Josh Mathiak <jmathiak4@gmail.com>
2023-05-31 10:27:49 -0500

    changed side bar + added a header for sidebar

--- src/components/BrowseShop.js
+++ src/components/BrowseShop.js
@@ -134,30 +134,37 @@ const BrowseShop = ({ cart, setCart }) => {
     },
   ];
   const [displayArr, setDisplayArr] = useState([...allItemArray]);
+  const [sorting, setSorting] = useState("All Items");
   const filterItems = (sort) => {
     let filteredArr = [];
     switch (sort) {
       default:
         setDisplayArr(allItemArray);
+        setSorting("All Items");
         break;
       case "all":
         setDisplayArr(allItemArray);
+        setSorting("All Items");
         break;
       case "vandals":
         filteredArr = [...allItemArray].filter((x) => x.type === "Vandal");
         setDisplayArr(filteredArr);
+        setSorting("Vandals");
         break;
       case "phantoms":
         filteredArr = [...allItemArray].filter((x) => x.type === "Phantom");
         setDisplayArr(filteredArr);
+        setSorting("Phantoms");
         break;
       case "sheriffs":
         filteredArr = [...allItemArray].filter((x) => x.type === "Sheriff");
         setDisplayArr(filteredArr);
+        setSorting("Sheriffs");
         break;
       case "ghosts":
         filteredArr = [...allItemArray].filter((x) => x.type === "Ghost");
         setDisplayArr(filteredArr);
+        setSorting("Ghosts");
         break;
     }
   };
@@ -222,43 +229,48 @@ const BrowseShop = ({ cart, setCart }) => {
   // }, [sortOption]);
   return (
     <div className="shop-area">
-      <ul className="shop-filter-options">
-        <li
-          onClick={() => {
-            filterItems("all");
-          }}
-        >
-          All Items
-        </li>
-        <li
-          onClick={() => {
-            filterItems("vandals");
-          }}
-        >
-          Vandals
-        </li>
-        <li
-          onClick={() => {
-            filterItems("phantoms");
-          }}
-        >
-          Phantoms
-        </li>
-        <li
-          onClick={() => {
-            filterItems("ghosts");
-          }}
-        >
-          Ghosts
-        </li>
-        <li
-          onClick={() => {
-            filterItems("sheriffs");
-          }}
-        >
-          Sheriffs
-        </li>
-      </ul>
+      <div className="shop-sidebar">
+        <h2 className="sort-header">
+          Shop / <br /> {sorting}
+        </h2>
+        <ul className="shop-filter-options">
+          <li
+            onClick={() => {
+              filterItems("all");
+            }}
+          >
+            All Items
+          </li>
+          <li
+            onClick={() => {
+              filterItems("vandals");
+            }}
+          >
+            Vandals
+          </li>
+          <li
+            onClick={() => {
+              filterItems("phantoms");
+            }}
+          >
+            Phantoms
+          </li>
+          <li
+            onClick={() => {
+              filterItems("ghosts");
+            }}
+          >
+            Ghosts
+          </li>
+          <li
+            onClick={() => {
+              filterItems("sheriffs");
+            }}
+          >
+            Sheriffs
+          </li>
+        </ul>
+      </div>
       <ItemGallery
         filterItems={filterItems}
         allItems={allItemArray}
