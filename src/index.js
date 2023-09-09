2e81d81114fa76c1961f84ea6ca591b490d05fae
Josh Mathiak <jmathiak4@gmail.com>
2023-09-08 20:04:03 -0500

    .

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import RouteSwitch from "./RouteSwitch";
 import { HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <App />
   </HashRouter>
 );
